14a7bba8eaaaebc46d5467e6aeda55ceaead3d29
Hemant Kumar <hemantkumarha1998@gmail.com>
2021-08-06 23:38:36 +0530

    New Edit, and Now it will be continued for later setup

--- index.php
+++ index.php
@@ -1,20 +1,205 @@
+
 <!DOCTYPE html>
-<html>
+<html lang="eng" version="5.2">
 <head>
-	<meta charset="utf-8">
-	<title>Home | IIITDM</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Introduction | Home</title>
+   <link rel="icon" type="jpg/png" href="https://img.icons8.com/cute-clipart/64/000000/h.png"/>
+   <script type="text/javascript" src="script.js"/></script>
+   <link rel="stylesheet" type="text/css" href="style.css">
 </head>
+
 <body>
-	<div class="container">
-		<nav class="navbar">
-			<ul>
-			<li><a href="/html/">HTML</a></li>
-			<li><a href="/css/">CSS</a></li>
-			<li><a href="/js/">JavaScript</a></li>
-			<li><a href="/python/">Python</a></li>
-		</ul>
-		</nav>
-	</div>
+
+<!-- NavBAr Page-->
+
+
+      <div class="navbar">
+            <ol>
+               <li><a class="button" onclick="openTabs(event,'id_03')" >Skills</a></li>
+               <li><a class="button" onclick="openTabs(event,'id_02')">Awards</a></li>
+               <li><a class="button" onclick="openTabs(event,'id_01')" id="defaultTabs">Home</a></li>
+               
+            </ol>
+      </div>
+
+               <br>
+               <br>
+
+
+<!-- Home Page-->
+
+   <div class="introduction" id="id_01">
+      <div class="heading">
+         <h1>
+         Hemant Kumar
+      </h1>
+      </div>
+         <hr>
+               <img class="photo" src="profile.png">
+      <div class="containt">
+            <p>
+               I am, a student of IIIT Jabalpur, from the department of <b><u>Computer Science and Engineering</u></b> enrolled in its 4 year BTech course. I am from Patna, Bihar. I completed my high school education from St. Karen's High School, Patna. After completion of my schooling, I attempted JEE Mains examination and got admission in IIIT. My hobbies include programming and web development, and in my free time I love to write new apps or create a new web application. Apart from coding, I have interest in electronics and I love to delve deep into the realm of electronic devices.
+            </p>
+            <p>
+               I participated in Litfest competition organised by <b><u>Litera Valley School</u></b> in Patna for senior secondary school students twice in <u><b><i>2018 and 2019 for Inter School Web development competition. </i></b></u><i>I secured the top position in 2019 while was selected runner up in 2018</i> .
+            </p>
+            <p>
+            I participated in Swift Playground competition which is organised by Apple Inc. in 2021 March, in which i have created small Swift Program Application . </p>
+            <p>
+               I have created 3 Mobile Applications, in which i have created 1 iOS Application and 2 Android Application, I also created one website for the Family College.
+            </p>
+      </div>
+   <br>
+</div>
+
+
+<!-- Awards Page-->
+
+
+ <div class="introduction" id= "id_02">
+      <div class="heading">
+         <h1>
+         Awards
+      </h1>
+      </div>
+         <hr>
+               <img class="photo" src="awards.jpeg">
+      <div class="containt">
+           <center>
+            <p>
+               I was honored with :-
+            </p>
+            </center>
+            <br>
+                  <table id="details">
+      <tr>
+         <th>Awards</th>
+         <th>Category</th>
+         <th>Result</th>
+         <th>Year</th>
+      </tr>
+      <tr>
+          <td>LitFest - 3(Inter-School)</td>
+          <td>Netizen Web Development</td>
+          <td>Runner Up (2<sup>nd</sup>)</td>
+          <td>2018</td>
+
+      </tr>
+      <tr>
+         <td>LitFest - 4(Inter-School)</td>
+          <td>Netizen Web Development</td>
+          <td>Winner Up (1<sup>st</sup>)</td>
+          <td>2019</td>
+      </tr>
+      <tr>
+         <td>InterClass Competition </td>
+          <td>Java Application</td>
+          <td>Second</td>
+          <td>2018-2019</td>
+      </tr>
+                  </table>
+
+         </div>
+   <br>
+</div>
+
+
+
+
+<!-- Skills Page-->
+
+<div class="introduction" id= "id_03">
+      <div class="heading">
+         <h1>
+         Skills
+      </h1>
+      </div>
+         <hr>
+               <img class="photo" src="Skills.jpeg" >
+      
+
+      <div class="containt">
+            <p padding="20px">
+               I have skills and knowledge in the Following:- 
+            </p>
+                  <br>
+            <dl>
+                        <dt>Html</dt>
+                              <dd>Basics and intermediate</dd>
+                        <dt>CSS</dt>
+                              <dd>All the Daily use Attributes</dd>
+                        <dt>JS</dt>
+                              <dd>Basic use and only for Web Development</dd>
+                        <dt>PhP</dt>
+                            <dd>Simple Use for form Action, database Connectivity</dd>
+                        <dt>MySQL</dt>
+                              <dd>Complete Course except data sorting</dd>
+                        <dt>Java</dt>
+                            <dd>All except Data structure Algorithm</dd>
+                            <dd>JDK Application with MySQL Backend</dd>
+                        <dt>C</dt>
+                            <dd>Core C</dd>
+                        <dt>Swift</dt>
+                           <dd>All except Data structure Algorithm</dd>
+                           <dd>Learning Phase</dd>
+                        <dt>iOS Development</dt>
+                           <dd>Creates Application</dd>
+                           <dd>Learning Phase</dd>
+                           <dd>Published Apps</dd>
+                         <dt>Android Development</dt>
+                           <dd>Creates Application</dd>
+                           <dd>Learning Phase</dd>
+                           <dd>Published Apps</dd>
+
+
+            </dl>
+
+      </div>
+   <br>
+</div>
+
+<!-- Skills Page Ends -->
+
+<br>
+
+
+
 </body>
+
+<!-- Body Ends -->
+<!-- Footer -->
+<div class="footer">
+                  <p align="center"> Follow Me on</p>
+                  <center>
+               <a href="https://www.instagram.com/its_hkrs" target="_blank"><img src="isnta.png"/></a>
+               <a href="https://twitter.com/its_HKRS" target="_blank"><img src="twitter.png"/></a>
+               <a href="https://github.com/Hemant2000-karens" target="_blank"><img src="gith.png"/></a>
+            </center>
+          <center>
+               <h5>Designed by <u><b>Hemant Kumar</b></u> | <a href="mailto:20bcs100@iiitdmj.ac.in">20bcs100@iiitdmj.ac.in</a>
+            </h5>
+         </center>
+
+      </div>
+
+<!-- Footer -->
+
+<script>
+function openTabs(evt, idNmane) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("introduction");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("button");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(idNmane).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("defaultTabs").click();
+</script>
 </html>
