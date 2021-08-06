77e2a93cb8bc31576a29e34d9cd1c24b720e3adb
Hemant Kumar <hemantkumarha1998@gmail.com>
2021-08-06 19:09:00 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Home | IIITDM</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="heading">
+		<a href="iiitdmj.ac.in">
+			<img src="logo_college.png" id="logo" />
+			<img src="icon.gif" id="name_of_ins" /> 
+		</a>
+	</div>
+</body>
+</html>
