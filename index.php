b7b1a10234992f51d4c61235340cee5136c41b59
Hemant Kumar <hemantkumarha1998@gmail.com>
2021-08-08 20:01:38 +0530

    Re-generated Files

--- index.php
+++ index.php
@@ -1,204 +1,48 @@
-
 <!DOCTYPE html>
-<html lang="eng" version="5.2">
-<head>
-   <meta charset="utf-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>IITDMJ | Home</title>
-   <link rel="icon" type="jpg/png" href="icon_title.png"/>
-   <script type="text/javascript" src="script.js"/></script>
-   <link rel="stylesheet" type="text/css" href="style.css">
-</head>
+<html>
+    <head>
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+            <link href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible&display=swap" rel="stylesheet">
+    </head>
+    
+    <body>
+    <div class="header">
+    <img src="logo_college.png"/>
+        <h1>
+        P.D.P.M<br>Indian Institute of Information Technology,<br>Design and Manufacturing, Jabalpur</h1>
+    </div>
+<hr>
 
-<body>
-
-<!-- NavBAr Page-->
-
-<div class="header">
-   <img src="logo_college.png" height="120px" width="120px" id="logo" style="padding-right:10px;"/>
-   <h3>P.D.P.M<br>Indian Institute of Information Technology,<br>Design and Manufacturing, Jabalpur
-</h3>
+<div class="topnav" id="myTopnav">
+  <a href="#home" class="active">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <div class="dropdown">
+    <button class="dropbtn">Dropdown 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </div> 
+  <a href="#about">About</a>
+  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
 </div>
 
-      <div class="navbar">
-            <ol>
-               <li><a class="button" ohref="#">Skills</a></li>
-               <li><a class="button" ohref="#">Awards</a></li>
-               <li><a class="button" ohref="#"id="defaultTabs">Home</a></li>
-               <li><a class="button" ohref="#">Dropdown</a>
-               		<ol>
-		               <li><a class="button" onclick="openTabs(event,'id_03')" >Skills</a></li>
-		               <li><a class="button" onclick="openTabs(event,'id_02')">Awards</a></li>
-		               <li><a class="button" onclick="openTabs(event,'id_01')" id="defaultTabs">Home</a></li>
-		               <li><a class="button" onclick="openTabs(event,'id_01')"></a></li>
-		               <li><a class="button" onclick="openTabs(event,'id_01')">Home</a></li>
-		               <li><a class="button" onclick="openTabs(event,'id_01')">Home</a></li>
-            		</ol>
-               </li>
-               <li><a class="button" onclick="openTabs(event,'id_01')">Dr2</a></li>
-               <li><a class="button" onclick="openTabs(event,'id_01')">Dr3</a></li>
-            </ol>
-      </div>
+    </body>
 
-               <br>
-               <br>
-
-
-<!-- Home Page-->
-
-   <div class="introduction" id="id_01">
-  <!--    <div class="heading">
-         <h1>
-         Hemant Kumar
-      </h1>
-      </div>
-         <hr>
-               <img class="photo" src="profile.png">
-      <div class="containt">
-            <p>
-               I am, a student of IIIT Jabalpur, from the department of <b><u>Computer Science and Engineering</u></b> enrolled in its 4 year BTech course. I am from Patna, Bihar. I completed my high school education from St. Karen's High School, Patna. After completion of my schooling, I attempted JEE Mains examination and got admission in IIIT. My hobbies include programming and web development, and in my free time I love to write new apps or create a new web application. Apart from coding, I have interest in electronics and I love to delve deep into the realm of electronic devices.
-            </p>
-            <p>
-               I participated in Litfest competition organised by <b><u>Litera Valley School</u></b> in Patna for senior secondary school students twice in <u><b><i>2018 and 2019 for Inter School Web development competition. </i></b></u><i>I secured the top position in 2019 while was selected runner up in 2018</i> .
-            </p>
-            <p>
-            I participated in Swift Playground competition which is organised by Apple Inc. in 2021 March, in which i have created small Swift Program Application . </p>
-            <p>
-               I have created 3 Mobile Applications, in which i have created 1 iOS Application and 2 Android Application, I also created one website for the Family College.
-            </p>
-      </div> -->
-   <br>
-</div>
-
-
-<!-- Awards Page-->
-
-
- <div class="introduction" id= "id_02">
-      <div class="heading">
-         <h1>
-         Awards
-      </h1>
-      </div>
-         <hr>
-               <img class="photo" src="awards.jpeg">
-      <div class="containt">
-           <center>
-            <p>
-               I was honored with :-
-            </p>
-            </center>
-            <br>
-                  <table id="details">
-      <tr>
-         <th>Awards</th>
-         <th>Category</th>
-         <th>Result</th>
-         <th>Year</th>
-      </tr>
-      <tr>
-          <td>LitFest - 3(Inter-School)</td>
-          <td>Netizen Web Development</td>
-          <td>Runner Up (2<sup>nd</sup>)</td>
-          <td>2018</td>
-
-      </tr>
-      <tr>
-         <td>LitFest - 4(Inter-School)</td>
-          <td>Netizen Web Development</td>
-          <td>Winner Up (1<sup>st</sup>)</td>
-          <td>2019</td>
-      </tr>
-      <tr>
-         <td>InterClass Competition </td>
-          <td>Java Application</td>
-          <td>Second</td>
-          <td>2018-2019</td>
-      </tr>
-                  </table>
-
-         </div>
-   <br>
-</div>
-
-
-
-
-<!-- Skills Page-->
-
-<div class="introduction" id= "id_03">
-      <div class="heading">
-         <h1>
-         Skills
-      </h1>
-      </div>
-         <hr>
-               <img class="photo" src="Skills.jpeg" >
-      
-
-      <div class="containt">
-            <p padding="20px">
-               I have skills and knowledge in the Following:- 
-            </p>
-                  <br>
-            <dl>
-                        <dt>Html</dt>
-                              <dd>Basics and intermediate</dd>
-                        <dt>CSS</dt>
-                              <dd>All the Daily use Attributes</dd>
-                        <dt>JS</dt>
-                              <dd>Basic use and only for Web Development</dd>
-                        <dt>PhP</dt>
-                            <dd>Simple Use for form Action, database Connectivity</dd>
-                        <dt>MySQL</dt>
-                              <dd>Complete Course except data sorting</dd>
-                        <dt>Java</dt>
-                            <dd>All except Data structure Algorithm</dd>
-                            <dd>JDK Application with MySQL Backend</dd>
-                        <dt>C</dt>
-                            <dd>Core C</dd>
-                        <dt>Swift</dt>
-                           <dd>All except Data structure Algorithm</dd>
-                           <dd>Learning Phase</dd>
-                        <dt>iOS Development</dt>
-                           <dd>Creates Application</dd>
-                           <dd>Learning Phase</dd>
-                           <dd>Published Apps</dd>
-                         <dt>Android Development</dt>
-                           <dd>Creates Application</dd>
-                           <dd>Learning Phase</dd>
-                           <dd>Published Apps</dd>
-
-
-            </dl>
-
-      </div>
-   <br>
-</div>
-
-<!-- Skills Page Ends -->
-
-<br>
-
-
-
-</body>
-
-<!-- Body Ends -->
-<!-- Footer -->
-<div class="footer">
-                  <p align="center"> Follow Me on</p>
-                  <center>
-               <a href="https://www.instagram.com/its_hkrs" target="_blank"><img src="isnta.png"/></a>
-               <a href="https://twitter.com/its_HKRS" target="_blank"><img src="twitter.png"/></a>
-               <a href="https://github.com/Hemant2000-karens" target="_blank"><img src="gith.png"/></a>
-            </center>
-          <center>
-               <h5>Designed by <u><b>Hemant Kumar</b></u> | <a href="mailto:20bcs100@iiitdmj.ac.in">20bcs100@iiitdmj.ac.in</a>
-            </h5>
-         </center>
-
-      </div>
-
-<!-- Footer -->
+          <script type="text/javascript">
+            function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+        </script>
 </html>
