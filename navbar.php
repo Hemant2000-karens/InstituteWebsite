f94962c9f827379a595ef57392c595d881935d8b
Hemant Kumar <hemantkumarha1998@gmail.com>
2021-08-22 22:07:51 +0530

    re changed the things

---
+++ navbar.php
@@ -0,0 +1,54 @@
+<div class="topnav" id="myTopnav">
+  <a href="#" class="active">Home</a>
+  <a href="#">Academics</a>
+  <a href="#">Contact</a>
+  <!-- DropDown Menu -->
+  <div class="dropdown">
+    <button class="dropbtn">Dropdown 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+<!-- Conent of the menu-->
+
+      <div class="row">
+          <div class="column">
+              <div class="dropdown-menu-content">
+            <h2>Mega Menu</h2>
+              </div> 
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+          <div class="column">
+            <h3>Category 2</h3>
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+          <div class="column">
+            <h3>Category 3</h3>
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+      </div>
+
+      
+
+    </div>
+  </div> 
+
+
+
+
+
+  <!-- DropDown Menu -->
+  <a href="#">Research</a>
+  <a href="#">About</a>
+  <a href="#about">About</a>
+  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">
+    <div class="bar1"/></div>
+  <div class="bar2"/></div>
+  <div class="bar3"/></div>
+</a>
+</div>
