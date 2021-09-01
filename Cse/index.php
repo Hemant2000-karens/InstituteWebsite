df7e49196cb6d695c654565cc1cf4512b1a4d713
Hemant Kumar <team.karensbook@icloud.com>
2021-09-01 14:28:36 +0530

    build 1.0
    new Update

--- Cse/index.php
+++ Cse/index.php
@@ -9,6 +9,15 @@
 	<link rel="icon" type="jpeg/png" href="">
 </head>
 <body>
+    <div class="header">
+    <img src="logo_college.png"/>
+        <h1>
+        P.D.P.M<br>Indian Institute of Information Technology,<br>Design and Manufacturing, Jabalpur</h1>
+    </div>
+<hr>
 
+<?php include "./navbar.php" ?>
+
+<span class="br"></span>
 </body>
 </html>
