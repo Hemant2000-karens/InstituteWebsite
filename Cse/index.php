c0d3755e171e05fe0bdebee904431045e3159a6a
Hemant Kumar <team.karensbook@icloud.com>
2021-09-01 14:10:31 +0530

    recovered Version
    check this
    
    Co-Authored-By: Shival Gupta <sgupta.5545@gmail.com>

---
+++ Cse/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>CSE | IIITDMJ</title>
+	<link rel="stylesheet" type="text/css" href="">
+	<link rel="stylesheet" type="text/css" href="">
+	<script type="text/javascript"></script>
+	<link rel="icon" type="jpeg/png" href="">
+</head>
+<body>
+
+</body>
+</html>
--- script.js
+++ script.js
@@ -2,7 +2,7 @@
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.classList.toggle("change");
-  x.style.background='#212121';
+  x.style.background='#6c705c';
     x.className += " responsive";
   } else {
     x.className = "topnav";
