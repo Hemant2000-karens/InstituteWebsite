f02fca1ef39978281e03b7617ed668140d00ac0a
Hemant Kumar <team.karensbook@icloud.com>
2021-10-16 13:50:19 +0530

    Review changes

--- topheader.php
+++ topheader.php
@@ -1,10 +1,16 @@
 <div class="topheader">
         <div class="headname">
-       <span class="hindi">भारत सरकार</span>
-       <span class="eng1">Government of India</span>
+                <span class="hindi">भारत सरकार</span>
+                <span class="eng1">Government of India</span>
        </div>
-       <div class="Ministry">
-       <span class="hindi">मानव संसाधन विकास मंत्रालय</span>
-       <span class="eng1">Ministry of Human Resources and Development</span>
-       </div>
+        <div class="Ministry">
+                <span class="hindi">मानव संसाधन विकास मंत्रालय</span>
+                <span class="eng1">Ministry of Human Resources and Development</span>
+        </div>
+       <div class="lang">
+        <ol>
+                <li><a class="button" onclick="openTabs(event,'id_03')" >Eng</a></li>
+                <li><a class="button" onclick="openTabs(event,'id_02')">हिन्दी</a></li>
+        </ol>
+        </div>
 </div>
