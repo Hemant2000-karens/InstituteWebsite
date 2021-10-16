26a5683be84c01e9ced8304ad2b827ac65b003aa
Hemant Kumar <team.karensbook@icloud.com>
2021-10-17 01:51:20 +0530

    pp

--- navbar.php
+++ navbar.php
@@ -12,12 +12,7 @@
       <a href="#">Link 1</a>
       <a href="#">Link 2</a>
       <a href="#">Link 3</a>
-        <a href="#" class="newMenu">Link 4</a>
-          <div class="finalClass">
-          <a href="">Link 5</a>
-          <a href="">Link 6</a>
-          <a href="">Link 7</a>
-          </div>
+      <a href="#" id="newMenu">Link 4</a>
     </div>
   </div> 
 
