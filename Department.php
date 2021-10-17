e63ee2c00096804b1379b05c2ebe630003318eca
Hemant Kumar <team.karensbook@icloud.com>
2021-10-17 10:49:47 +0530

    added new PAge
    Department.php

---
+++ Department.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Department</title>
+		<meta http-equiv="refresh" content="3000">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+       <link rel="stylesheet" type="text/css" href="mobile.css">
+      <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap" rel="stylesheet">
+</head>
+	    <?include 'topheader.php';?>
+	    <body>
+    <div class="header">
+    <img src="logo_college.png"/>
+        <h1>
+        P.D.P.M<br>Indian Institute of Information Technology,<br>Design and Manufacturing, Jabalpur</h1>
+    </div>
+<hr>
+
+<?php include "./navbar.php" ?>
+
+<span class="br"></span>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title></title>
+        <title>IIITDMJ | Home</title>
          <meta http-equiv="refresh" content="3000">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="style.css">
