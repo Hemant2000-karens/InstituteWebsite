1780aa8e81f2ca8fffe1b463189f039e8b0bc558
Hemant Kumar <team.karensbook@icloud.com>
2021-11-27 19:49:59 +0530

    needs to change message page

--- en/messageDesk.php
+++ en/messageDesk.php
@@ -1,14 +1,16 @@
 <div class="messageDesk">
 	<div class="mesageContent">
+
+		<div class="messageTitle">
+		 	<span class="mesage">Message from</span><span class="desk">Desk</span>
+		 </div>
 		 <div class="personImage">
 		 	<img src="./Images/director_msg.jpg"/>
 		 </div>
 		 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-		 <div class="messageTitle">
-		 	<span class="mesage">Message from</span><span class="desk">Desk</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 		 <div class="chButton">
 		 	<a href="">&lt;</a><a href="">&gt;</a>
-		 </div>
 		 </div>
 		 <div class="designation">
 		 	<span>Director</span><span>Dean</span>
