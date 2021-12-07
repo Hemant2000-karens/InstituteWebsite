a44daca6984b3afb2c569a2d738826a16543f0dd
Rishabh Dev <devrishabh28@gmail.com>
2021-12-07 13:21:56 +0530

    Improved design and structure of notice and added it's script

--- en/notice.php
+++ en/notice.php
@@ -4,12 +4,43 @@
 		<span style="padding: 0 18px;"><a href="#">View All</a></span>
 	</div>
 
-	<div class="noticeContent">
-		<img class="prev" src="./Images/left_arr.png"/>
 
-			<div class="centerNotice">
-				Hello I am here
-			</div>
-		<img src="./Images/right_Arr.png" class="forw"/>
+	
+	<div class="noticeSlider">
+	<img class="prev" src="./Images/left_arr.png"/>
+	<div class="noticeContent">Hello I am here</div>
+	<div class="noticeContent">You are not</div>
+	<div class="noticeContent">Hello I am here 2</div>
+	<div class="noticeContent">You are not 2</div>
+	<div class="noticeContent">Hello I am here 3</div>
+	<div class="noticeContent">You are not 3</div>
+	<div class="noticeContent">Hello I am here 4</div>
+	<div class="noticeContent">You are not 4</div>
+	<div class="noticeContent">Hello I am here 5</div>
+	<div class="noticeContent">You are not 5</div>
+	
+	
+	<img src="./Images/right_Arr.png" class="forw"/>
 	</div>
 </div>
+
+<script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("noticeContent");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "inline-flex";
+  slides[slideIndex].style.display = "inline-flex";  
+  
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+
+}
+</script>
