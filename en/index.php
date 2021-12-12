6f14dacb43e6f44742d6500345ae01f5c7ce69e2
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2021-12-13 00:08:08 +0530

    slightly bugs fixed
    good night, going to sleep

--- en/index.php
+++ en/index.php
@@ -48,24 +48,6 @@
 <?php include './messageDesk.php'; ?>
 </div>
 
-
-
- <script>
-   var myIndex = 0;
-carousel();
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("SlideShowImage");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}
-  x[myIndex-1].style.display = "block";
-  setTimeout(carousel, 7000); // Change image every 2 seconds
-}
- </script>
  <?php include './footer.php';?>
 </body>
 </html>
