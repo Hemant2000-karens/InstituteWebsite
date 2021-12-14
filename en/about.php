589b8a1c4e0674e5cbb20d0becf4feff11ef8183
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2021-12-14 22:01:13 +0530

    section on

--- en/about.php
+++ en/about.php
@@ -2,26 +2,26 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Academics | IIITDMJ</title>
+	<title>About Us | IIITDMJ</title>
 		<meta http-equiv="refresh" content="3000">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./style.css">
        <link rel="stylesheet" type="text/css" href="./mobile.css">
 </head>
-
-		<?php include './topheader.php';?>
-	    <div class="container">
-	    <body>
-	    	<?php include './Header.php';?>
-
+	<?php include './topheader.php';?>
+	<div class="container">
+		<body>
+			<?php include './Header.php';?>
 			<?php include "./navbar.php" ?>
 
-<span class="br"></span>
-	
+			<span class="br"></span>
+			<section>
+				
+			</section>
 
 
-	</body>
-</div>
+		</body>
+	</div>
 
 <?php include './footer.php'?>
 </html>
