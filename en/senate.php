93b81276d70f615f16b5cde1575061a72b534ea7
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-01-22 21:57:26 +0530

    v2.3.8
    Bugs fix, and now plan changed to build new admin, Local admin, and senate page.

---
+++ en/senate.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>BWC | IIITDMJ</title>
+		<meta http-equiv="refresh" content="3000">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./style.css">
+       <link rel="stylesheet" type="text/css" href="./mobile.css">
+</head>
+	<?php include './topheader.php';?>
+	<div class="container">
+		<body>
+			<?php include './Header.php';?>
+			<?php include "./navbar.php" ?>
+
+			<span class="br"></span>
+
+			<div class="aboutHeader">
+				<h2>Senate</h2>
+			</div>
+
+
+			<section class="secondAbout">
+				<div class="otherPage">
+					<div class="card" onclick="linkOpen('bod')">
+						<img src="./icons/about.png"/>
+						<figcaption>About Us</figcaption>
+					</div>
+					<div class="card" onclick="linkOpen('adm')">
+						<img src="./icons/admins.png"/>
+						<figcaption>Administration</figcaption>
+					</div>
+					<div class="card" onclick="linkOpen('lad')">
+						<img src="./icons/locala.png"/>
+						<figcaption>Local Administration</figcaption>
+					</div>
+					</div>
+					<div class="otherPage">
+						<div class="card" onclick="linkOpen('ms')">
+							<img src="./icons/senate.png"/>
+						<figcaption>Members of Senate</figcaption>
+						</div>
+						<div class="card" onclick="linkOpen('fc')">
+							<img src="./icons/finc.png"/>
+						<figcaption>Finance Commitee</figcaption>
+						</div>
+						<div class="card" onclick="linkOpen('mv')">
+							<img src="./icons/mivi.png"/>
+						<figcaption>Mission & Vision</figcaption>
+						</div>
+				</div>
+			</section>
+
+		</body>
+<div>
+	
+</div>
+
+	</div>
+	//body ends here
+
+<?php include './footer.php'?>
+
+<script type="text/javascript">
+	var button = document.getElementsByClassName("card");
+	button.addEventListener("Click",linkOpen);
+
+
+	function linkOpen(str)
+	{
+		switch(str)
+		{
+			case 'bod':
+				window.open('https://www.google.com', '_blank');
+				break;
+			case 'adm':
+				window.open('https://www.facebook.com', '_blank');
+				break;
+			case 'lad':
+				window.open('https://www.instagram.com', '_blank');
+				break;
+			case 'ms':
+				window.open('https://www.twitter.com', '_blank');
+				break;
+			case 'fc':
+				window.open('https://www.linkedin.com', '_blank');
+				break;
+			case 'mv':
+				window.open('https://www.oracle.com', '_blank');
+				break;
+		}
+
+	}
+</script>
+</html>
