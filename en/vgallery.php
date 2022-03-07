a675c733278eeae17356f93176bb9074d475b248
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-03-07 23:25:55 +0530

    updated Video gallery
    New Update on Video Gallery

--- en/vgallery.php
+++ en/vgallery.php
@@ -14,19 +14,48 @@
 		<?php include "./navbar.php" ?>
 		<span class="br"></span>
 
+<div class="vcontainer">
+
+	<div class="vCaption">
+		<iframe width="640" height="400" src="https://www.youtube.com/embed/_4X9CcLmo7w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+		</iframe>
+
+		<span class="text">
+			DeW 8.0 :Future of Design Thinking and 3D Printing in Healthcare: 3rd day 
+		</span>
+	</div>
+
+
+<div class="vCaption">
+		<iframe width="640" height="400" src="https://www.youtube.com/embed/i1bjtCPNkwo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+		</iframe>
+		<span class="text">
+			Van Mahotsav 2021 || PDPM IIITDM Jabalpur 
+		</span>
+	</div>
+
+<div class="vCaption">
+		<iframe width="640" height="400" src="https://www.youtube.com/embed/793FXs-LiT4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+		</iframe>
+		<span class="text">
+			Celebration of International Yoga Day 2021i
+		</span>
+	</div>
+
+<div class="vCaption">
+		<iframe width="640" height="400" src="https://www.youtube.com/embed/P1DHOVwkTsA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+		</iframe>
+		<span class="text">
+			11th Convocation IIITDM Jabalpur 
+		</span>
+	</div>
 
 
 
 
 
 
-
-
-
-
-
-
-
+</div>
 
 	</body>
 </div>
