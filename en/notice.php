4ad205a962e9d7257c81dd11b2ee7c72a60eb1f3
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-03-10 00:34:22 +0530

    updated Notice page

--- en/notice.php
+++ en/notice.php
@@ -1,115 +1,116 @@
-<div class="noticebody">
-	<div class="noticeTitle">
-		<span>News & <strong>Notice</strong></span>
-		<span style="padding: 0 18px;"><a href="viewall.php">View All</a></span>
-	</div>
+<div class="noticeMain">
+		<div class="noticebody">
+			<div class="noticeTitle">
+				<span>News & <strong>Notice</strong></span>
+				<span style="padding: 0 18px;"><a href="viewall.php">View All</a></span>
+			</div>
 
-	<div class="noticeSlider">
-	<img class="prev" src="./Images/left_arr.png" onclick="plusSlides(-1)" />
+			<div class="noticeSlider">
+			<img class="prev" src="./Images/left_arr.png" onclick="plusSlides(-1)" />
 
 
-	<div class="noticeContent">
-	<p>
-		<img src="./icons/clock.png" alt= "date"/>
-		<span class="date">8
-			<sup style="text-transform: lowercase">th</sup>
-			May,2021
-		</span>
-		<span class="message">
-		Hello I am here 1
-		</span>
-	</p>
-	</div>
-	<div class="noticeContent">
-	<p>
-		<img src="./icons/clock.png" alt= "date"/>
-		<span class="date">9
-			<sup style="text-transform: lowercase">th</sup>
-			May,2021
-		</span>
-		<span class="message">
-		Hello I am here 2
-		</span>
-	</p>
-	</div>
-	<div class="noticeContent">
-	<p>
-		<img src="./icons/clock.png" alt= "date"/>
-		<span class="date">10
-			<sup style="text-transform: lowercase">th</sup>
-			May,2021
-		</span>
-		<span class="message">
-		Hello I am here 3
-		</span>
-	</p>
-	</div>
-	<div class="noticeContent">
-	<p>
-		<img src="./icons/clock.png" alt= "date"/>
-		<span class="date">11
-			<sup style="text-transform: lowercase">th</sup>
-			May,2021
-		</span>
-		<span class="message">
-		Hello I am here 4
-		</span>
-	</p>
-	</div>
-	<div class="noticeContent">
-	<p>
-		<img src="./icons/clock.png" alt= "date"/>
-		<span class="date">12
-			<sup style="text-transform: lowercase">th</sup>
-			May,2021
-		</span>
-		<span class="message">
-		Hello I am here 5
-		</span>
-	</p>
-	</div>
-	<div class="noticeContent">
-	<p>
-		<img src="./icons/clock.png" alt= "date"/>
-		<span class="date">13
-			<sup style="text-transform: lowercase">th</sup>
-			May,2021
-		</span>
-		<span class="message">
-		Hello I am here 6
-		</span>
-	</p>
-	</div>
-	<div class="noticeContent">
-	<p>
-		<img src="./icons/clock.png" alt= "date"/>
-		<span class="date">14
-			<sup style="text-transform: lowercase">th</sup>
-			May,2021
-		</span>
-		<span class="message">
-		Hello I am here 7
-		</span>
-	</p>
-	</div>
-	
-	<div class="noticeContent">
-	<p>
-		<img src="./icons/clock.png" alt= "date"/>
-		<span class="date">15
-			<sup style="text-transform: lowercase">th</sup>
-			May,2021
-		</span>
-		<span class="message">
-		Hello I am here 8
-		</span>
-	</p>
-	</div>
+			<div class="noticeContent">
+			<p>
+				<img src="./icons/clock.png" alt= "date"/>
+				<span class="date">8
+					<sup style="text-transform: lowercase">th</sup>
+					May,2021
+				</span>
+				<span class="message">
+				Hello I am here 1
+				</span>
+			</p>
+			</div>
+			<div class="noticeContent">
+			<p>
+				<img src="./icons/clock.png" alt= "date"/>
+				<span class="date">9
+					<sup style="text-transform: lowercase">th</sup>
+					May,2021
+				</span>
+				<span class="message">
+				Hello I am here 2
+				</span>
+			</p>
+			</div>
+			<div class="noticeContent">
+			<p>
+				<img src="./icons/clock.png" alt= "date"/>
+				<span class="date">10
+					<sup style="text-transform: lowercase">th</sup>
+					May,2021
+				</span>
+				<span class="message">
+				Hello I am here 3
+				</span>
+			</p>
+			</div>
+			<div class="noticeContent">
+			<p>
+				<img src="./icons/clock.png" alt= "date"/>
+				<span class="date">11
+					<sup style="text-transform: lowercase">th</sup>
+					May,2021
+				</span>
+				<span class="message">
+				Hello I am here 4
+				</span>
+			</p>
+			</div>
+			<div class="noticeContent">
+			<p>
+				<img src="./icons/clock.png" alt= "date"/>
+				<span class="date">12
+					<sup style="text-transform: lowercase">th</sup>
+					May,2021
+				</span>
+				<span class="message">
+				Hello I am here 5
+				</span>
+			</p>
+			</div>
+			<div class="noticeContent">
+			<p>
+				<img src="./icons/clock.png" alt= "date"/>
+				<span class="date">13
+					<sup style="text-transform: lowercase">th</sup>
+					May,2021
+				</span>
+				<span class="message">
+				Hello I am here 6
+				</span>
+			</p>
+			</div>
+			<div class="noticeContent">
+			<p>
+				<img src="./icons/clock.png" alt= "date"/>
+				<span class="date">14
+					<sup style="text-transform: lowercase">th</sup>
+					May,2021
+				</span>
+				<span class="message">
+				Hello I am here 7
+				</span>
+			</p>
+			</div>
+			
+			<div class="noticeContent">
+			<p>
+				<img src="./icons/clock.png" alt= "date"/>
+				<span class="date">15
+					<sup style="text-transform: lowercase">th</sup>
+					May,2021
+				</span>
+				<span class="message">
+				Hello I am here 8
+				</span>
+			</p>
+			</div>
 
-	<img src="./Images/right_arr.png" class="forw" onclick="plusSlides(1)"/>
-	</div>
+			<img src="./Images/right_arr.png" class="forw" onclick="plusSlides(1)"/>
+			</div>
+		</div>
 </div>
-
 
 
 <script type="text/javascript">
