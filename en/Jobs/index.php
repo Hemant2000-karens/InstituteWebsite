1bf3ca3e1fd80d564b6fb3be3305e0167fcff0a5
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-04-08 18:32:55 +0530

    fix

--- en/Jobs/index.php
+++ en/Jobs/index.php
@@ -2,11 +2,11 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Jobs@IIITDMJ</title>
 	<link rel="stylesheet" type="text/css" href="./style.css">
 	<link rel="stylesheet" type="text/css" href="./mobile.css">
-
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <?php include './navbar.php'; ?>
 <body>
