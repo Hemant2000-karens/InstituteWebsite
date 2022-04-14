006bb29691f54d2fd1fb0cb47f63edcdabc1240c
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-04-14 14:04:54 +0530

    completed BOG, and Finc

--- en/bod.php
+++ en/bod.php
@@ -23,6 +23,7 @@
 			<div class="Directors">
 				<div class="profile">
 					<div class="members">
+						<img src="./Images/BOD/dpgs.png">
 						<h3>Shri Deepak Ghaisas</h3>
 					     <p>Chairman of the Board</p>
 					</div>
@@ -37,6 +38,7 @@
 					     <p>Additional Secretary<br>(IITs/IIITs/<br>TE/TC/TEQUIP)</p>
 					</div>
 					<div class="members">
+						<img src="./Images/BOD/jpm.png">
 						<h3>Dr. Jaideep Kumar Mishra</h3>
 					     <p>Member</p>
 					     <p>Joint Secretary & Group Coordinator<br>Ministry of Electronics and Information Technology</p>
@@ -44,6 +46,7 @@
 
 
 					<div class="members">
+						<img src="./Images/BOD/prvj.png">
 						<h3>Prof. R. V. Raja kumar</h3>
 					     <p>Member</p>
 					     <p> Director<br>Indian Institute of Technology Bhubaneswar</p>
@@ -52,26 +55,31 @@
 
 				<div class="profile">
 					<div class="members">
+						<img src="./Images/BOD/psh.png">
 						<h3>Prof. Shailendra Singh</h3>
 					     <p>Member</p>
 					     <p>Director<br>IIM Ranchi</p>
 					</div>
 					<div class="members">
+						<img src="./Images/BOD/ABT.png">
 						<h3>Ms. Atreyee Borooah Thekedath</h3>
 					     <p>Member</p>
 					     <p>Director<br>Web.com (India) Pvt. Ltd.</p>
 					</div>
 					<div class="members">
+						<img src="./Images/BOD/pkp.png">
 						<h3>Shri Prashant Pole</h3>
 					     <p>Member</p>
 					     <p>Director<br>Disha Consultants, Jabalpur</p>
 					</div>
 					<div class="members">
+						<img src="./Images/BOD/subsmy.png">
 						<h3>Shri Subrahmanya S V</h3>
 					     <p>Member</p>
 					     <p>Former VC<br>Infosys Ltd. Bengluru</p>
 					</div>
 					<div class="members">
+						<img src="./Images/BOD/pkjain.png">
 						<h3>Prof. Prashant K. Jain</h3>
 					     <p>Member</p>
 					     <p>Professor<br>PDPM IIITDM Jabaplpur</p>
@@ -80,16 +88,19 @@
 
 				<div class="profile">
 					<div class="members">
+						<img src="./Images/BOD/sns.png">
 						<h3>Prof. Sanjeev N. Sharma</h3>
 					     <p>Member</p>
 					     <p>Professor<br>PDPM IIITDM Jabaplpur</p>
 					</div>
 					<div class="members">
+						<img src="./Images/BOD/pnkon.png">
 						<h3>Prof. P.N Kondekar</h3>
 					     <p>Member</p>
 					     <p>Director(I/C)<br>PDPM IIITDM Jabaplpur</p>
 					</div>
 					<div class="members">
+						<img src="./Images/BOD/swapnali.png">
 						<h3>Ms. Swapnali D.Gadekar</h3>
 					     <p>Secretary</p>
 					     <p>Acting Registrar<br>PDPM IIITDM Jabalpur
--- en/finc.php
+++ en/finc.php
@@ -23,17 +23,20 @@
 			<div class="Directors">
 				<div class="profile">
 					<div class="members">
+						<img src="./Images/FC/dpgs.png">
 						<h3>Shri Deepak Ghaisas</h3>
 					     <p>Chairman of the Board</p>
 					</div>
 
-          <div class="members">
+          			<div class="members">
+          				<img src="./Images/FC/ABT.png">
 						<h3>Ms. Atreyee Borooah Thekedath</h3>
-					     <p>Member</p>
-					     <p>Director<br>Web.com (India) Pvt. Ltd.</p>
+					    <p>Member</p>
+					    <p>Director<br>Web.com (India) Pvt. Ltd.</p>
 					</div>
 
 					<div class="members">
+						<img src="./Images/FC/pkp.png">
 						<h3>Shri Prashant Pole</h3>
 					     <p>Member</p>
 					     <p>Director<br>Disha Consultants, Jabalpur</p>
@@ -54,12 +57,18 @@
 					     <p>Member</p>
 					     <p>Director(Finance)<br>Integrated Finance Division</p>
 					</div>
-          <div class="members">
+
+
+          			<div class="members">
+          				<img src="./Images/FC/pnkon.png">
 						<h3>Prof. P.N Kondekar</h3>
 					     <p>Member</p>
 					     <p>Director(I/C)<br>PDPM IIITDM Jabaplpur</p>
 					</div>
-          <div class="members">
+
+
+          			<div class="members">
+          				<img src="./Images/FC/swapnali.png">
 						<h3>Ms. Swapnali D.Gadekar</h3>
 					     <p>Secretary</p>
 					     <p>Acting Registrar<br>PDPM IIITDM Jabalpur
