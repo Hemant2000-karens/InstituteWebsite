6eb81884cd235de892d0a7f56c883b080ed44616
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-04-15 12:01:34 +0530

    updated

--- en/notice.php
+++ en/notice.php
@@ -1,4 +1,7 @@
 <div class="item1">
+
+
+
 <?php include './connectionDB.php';
 
 	$sql_query = "SELECT * FROM Notice";
@@ -10,14 +13,23 @@
 	{
 		while($rows = $result -> fetch_assoc())
 		{
-			echo "<br> <a href = '".$rows['notice_link']."'"."target='_blank'>".$rows['notice_text']."</a>"."-".$rows['date']."<br>";
+			echo "<div class='item1Content'>";
+			echo "<span class='br'></span><a href = '".$rows['notice_link']."'"."target='_blank'>".$rows['notice_text']."</a>"."<span class='br'></span> <div class='notice_date'> &nbsp; - ".$rows['date']."</div>";
+			echo "</div> <hr> ";
 		}
+
+		echo "<div class='item1Content0'>";
+			echo "<span class='br'></span><a href = '"."./viewall.php"."'"."target='_blank'>"."View All"."</a>";
+			echo "</div>";
 	}
 	else
 	{
-		echo "View all - <a href='viewall.php'>View All</a>";
+		echo "<div class='item1Content0'>";
+		echo "<span class='br'></span><a href = '"."./viewall.php"."'"."target='_blank'>"."View All"."</a>";
+		echo "</div>";
 	}
 
 
 ?>
+
 </div>
