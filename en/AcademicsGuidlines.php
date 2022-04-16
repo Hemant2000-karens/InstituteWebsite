2b3f66806738ebb6fb7cc478b480ffe171aa12c5
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-04-16 14:28:48 +0530

    fix

---
+++ en/AcademicsGuidlines.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Academics | IIITDMJ</title>
+		<meta http-equiv="refresh" content="3000">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./style.css">
+       <link rel="stylesheet" type="text/css" href="./mobile.css">
+</head>
+
+	    <?php include './topheader.php';?>
+	    <div class="container">
+	    <body>
+	    	<?php include './Header.php';?>
+
+			<?php include "./navbar.php" ?>
+
+<span class="br"></span>
+	
+	<div class="AcMain">
+		<div class="AcademicsNav">
+			<aside>
+				<div class="AcTitle">
+					Academic programmes
+				</div>
+					<li>B.Tech</li>
+					<li>M.Tech</li>
+					<li>B.Des</li>
+					<li>M.Des</li>
+					<li>PhD</li>
+					<li>Integrated Master + PhD</li>
+					<li>Special Part time M.Tech/M.Des</li>
+				<div class="AcTitle">
+					Admission
+				</div>
+					<li>B.Tech</li>
+					<li>M.Tech</li>
+					<li>B.Des</li>
+					<li>M.Des</li>
+					<li>PhD</li>
+					<li>Integrated Master + PhD</li>
+					<li>Special Part time M.Tech/M.Des</li>
+				<div class="AcTitle">
+					Important Documents
+				</div>
+					
+			</aside>
+		</div>
+		<div class="AcContents">
+			<section>
+				
+			</section>
+		</div>
+	</div>
+</body>
+</div>
+
+<?php include './footer.php'?>
+</html>
