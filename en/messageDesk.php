b96f8e9d388c2f283257e86a6cb81d0fe08dbe2d
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-05-01 14:29:22 +0530

    comit

--- en/messageDesk.php
+++ en/messageDesk.php
@@ -5,6 +5,17 @@
 	</div>
 
 	<div class="messageDeskCombined">
+			<img class="imagedirector" src="./Images/director_msg.jpeg" alt="Director Image" width="600" height="400">
+
+		<div class="deskSpeech">
+
+
+			<img src="./icons/opquote.png" style = "height:20px; width: 20px">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+		<img src="./icons/clquote.png" style ="height:20px; width:20px;">
+		 ~ (Acting)
 		
+		</div>
 	</div>
 </div>
