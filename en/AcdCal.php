fc34b9c1edf9ec0935076b84bb928db1963e27af
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-05-04 19:48:10 +0530

    u

--- en/AcdCal.php
+++ en/AcdCal.php
@@ -23,27 +23,103 @@
 				</div>
 
 				<div class="AcMain">
+					<div class="calMain">
+						<h3>
+							Academic Calendar for Session 
+							<?php 
+							include 'connectionDB.php';
+							printf("2022-2023");
+    					mysqli_close($link);
+							?>
+						</h3>
+					</div>
+						<div class="calTable">
+							<table id="tb1">
+								 <caption>Semester &#x2160;</caption>
+								  <tr>
+    								<th>Events</th>
+    								<th>Previous UG, PG, <span class="br"></span> New M.Des, & New P.hD</th>
+    								<th>M.tech</th>
+    								<th>New UG</th>
+  								</tr>
+  								<!-- Heading of the tables-->
 
-					
+
+
+  								<tr>
+    								<td>Pre-Registration</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Online Registration/ Physical Reporting at the Institute [only if applicable]</td>
+    								<td>Late Physical reporting</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Pre-Registration</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Pre-Registration</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+
+							</table>
+							<table id="tb2">
+								 <caption>Semester &#x2161;</caption>
+								  <tr>
+    								<th>Events</th>
+    								<th>Previous UG, PG, <span class="br"></span> New M.Des, & New P.hD</th>
+    								<th>M.tech</th>
+    								<th>New UG</th>
+  								</tr>
+  									<!-- Heading of the tables-->
+
+  								<tr>
+    								<td>Events</td>
+    								<td>Previous UG</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Pre-Registration</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Pre-Registration</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Pre-Registration</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+
+							</table>
+						</div>
 				</div>
 			</body>
 		</div>
 
 			<?php include './footer.php'?>
-		<script>
-		var coll = document.getElementsByClassName("acdpB");
-		var i;
-
-		for (i = 0; i < coll.length; i++) {
-		  coll[i].addEventListener("click", function() {
-		    this.classList.toggle("show");
-		    var content = this.nextElementSibling;
-		    if (content.style.maxHeight){
-		      content.style.maxHeight = null;
-		    } else {
-		      content.style.maxHeight = content.scrollHeight + "px";
-		    } 
-		  });
-		}
-		</script>
 </html>
