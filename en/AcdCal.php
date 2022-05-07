55e612ea8d05d00526141d4d58622cdaa035d74c
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-05-07 14:43:19 +0530

    push

--- en/AcdCal.php
+++ en/AcdCal.php
@@ -40,7 +40,7 @@
     								<th>Events</th>
     								<th>Previous UG, PG, <span class="br"></span> New M.Des, & New P.hD</th>
     								<th>M.tech</th>
-    								<th>New UG</th>
+    								<th>New UG Batch</th>
   								</tr>
   								<!-- Heading of the tables-->
 
@@ -55,45 +55,250 @@
 
   								<tr>
     								<td>Online Registration/ Physical Reporting at the Institute [only if applicable]</td>
-    								<td>Late Physical reporting</td>
-    								<td>M.tech + P.hD</td>
-    								<td>New UG</td>
-  								</tr>
-
-  								<tr>
-    								<td>Pre-Registration</td>
     								<td>April 06-10, 2020 (Mon-Fri)</td>
     								<td>M.tech + P.hD</td>
     								<td>New UG</td>
   								</tr>
 
   								<tr>
-    								<td>Pre-Registration</td>
+    								<td>Late Physical Reporting</td>
     								<td>April 06-10, 2020 (Mon-Fri)</td>
     								<td>M.tech + P.hD</td>
     								<td>New UG</td>
   								</tr>
 
+  								<tr>
+    								<td>Orientation of New Students</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Commencement of Classes</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Last Date for Adding of courses</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+  								<tr>
+    								<td>Last Date for Dropping of courses</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+  								<tr>
+    								<td>Last Date for Documents submission(New Admission)</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Mid-Sem Examination</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>Feb 11, 2022(Fri)</td>
+  								</tr>
+
+  								<tr>
+    								<td>Mid-Sem Recess</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>Jan 24-29, 2022(Mon-Sat)</td>
+  								</tr>
+
+  								<tr>
+    								<td>
+    									Last Date for showing Mid-Sem answer sheets to students & display of<b>compiled marks upto Mid-Semester</b>
+    								</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Classes Ends</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>End Semester Examination</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>PG Presentation(Graduation/Research/Progress Seminar)</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Last Date for showing of compiled marks(Full Semester)</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Last Date of End Semester Grades submission for Courses</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+  								<tr>
+    								<td>Make-up Examination</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+  								<tr>
+    								<td>Last Date for Grades Submission of Make-up Exams, Conversion of I Grades, Graduate/Research/Progress Seminars</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+  								<tr>
+    								<td>Submission of grade moderation report from discipline</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+  								<tr>
+    								<td>Submission of moderation report by APCS</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+  								<tr>
+    								<td>Last date for Authentication of Grades</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+  								<tr>
+    								<td>Academic Pre-Registration for next semester</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Vacation for UG students</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  									<tr>
+    								<td>Vacational leave applicable for faculty</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
 
 							</table>
+
+							<div class="calNotice">
+
+								<h4><sup>*</sup>It is mandatory to show answersheets and mark to students within 10 days of the evaluation</h4>
+
+
+								<table>
+
+									<tr>
+										<th colspan="3">Adjustment of Classes on Saturday</th>
+									</tr>
+
+									<tr>
+										<td>
+											Monday Time table
+										</td>
+										<td>
+											19/09/2020 & 24/10/2020
+										</td>
+										<td>
+											28/11/2020 & 16/01/2021
+										</td>
+									</tr>
+
+									<tr>
+										<td>
+											Tuesday Time table
+										</td>
+										<td>
+											26/09/2020 & 07/11/2020
+										</td>
+										<td>
+											05/12/2020 & 23/01/2021
+										</td>
+									</tr>
+
+									<tr>
+										<td>
+											Wednesday Time table
+										</td>
+										<td>
+											03/10/2020, 14/11/2020 & 05/12/2020
+										</td>
+										<td>
+											12/12/2020 & 30/01/2021
+										</td>
+									</tr>
+
+									<tr>
+										<td>
+											Thursday Time table
+										</td>
+										<td>
+											10/10/2020 & 21/11/2020
+										</td>
+										<td>
+											26/12/2020 & 06/02/2021
+										</td>
+									</tr>
+
+									<tr>
+										<td>
+											Friday Time table
+										</td>
+										<td>
+											17/10/2020 & 28/11/2020
+										</td>
+										<td>
+											02/01/2021 & 13/02/2021
+										</td>
+									</tr>
+
+								</table>
+
+							</div>
+
+
+
+
 							<table id="tb2">
 								 <caption>Semester &#x2161;</caption>
 								  <tr>
     								<th>Events</th>
     								<th>Previous UG, PG, <span class="br"></span> New M.Des, & New P.hD</th>
     								<th>M.tech</th>
-    								<th>New UG</th>
+    								<th>New UG Batch</th>
   								</tr>
   									<!-- Heading of the tables-->
 
-  								<tr>
-    								<td>Events</td>
-    								<td>Previous UG</td>
-    								<td>M.tech + P.hD</td>
-    								<td>New UG</td>
-  								</tr>
-
-  								<tr>
+  									<tr>
     								<td>Pre-Registration</td>
     								<td>April 06-10, 2020 (Mon-Fri)</td>
     								<td>M.tech + P.hD</td>
@@ -101,21 +306,237 @@
   								</tr>
 
   								<tr>
-    								<td>Pre-Registration</td>
+    								<td>Online Registration/ Physical Reporting at the Institute [only if applicable]</td>
     								<td>April 06-10, 2020 (Mon-Fri)</td>
     								<td>M.tech + P.hD</td>
     								<td>New UG</td>
   								</tr>
 
   								<tr>
-    								<td>Pre-Registration</td>
+    								<td>Late Physical Reporting</td>
     								<td>April 06-10, 2020 (Mon-Fri)</td>
     								<td>M.tech + P.hD</td>
     								<td>New UG</td>
   								</tr>
 
+  								<tr>
+    								<td>Orientation of New Students</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Commencement of Classes</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Last Date for Adding of courses</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+  								<tr>
+    								<td>Last Date for Dropping of courses</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+  								<tr>
+    								<td>Last Date for Documents submission(New Admission)</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Mid-Sem Examination</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>Feb 11, 2022(Fri)</td>
+  								</tr>
+
+  								<tr>
+    								<td>Mid-Sem Recess</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>Jan 24-29, 2022(Mon-Sat)</td>
+  								</tr>
+
+  								<tr>
+    								<td>
+    									Last Date for showing Mid-Sem answer sheets to students & display of<b>compiled marks upto Mid-Semester</b>
+    								</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Classes Ends</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>End Semester Examination</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>PG Presentation(Graduation/Research/Progress Seminar)</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Last Date for showing of compiled marks(Full Semester)</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Last Date of End Semester Grades submission for Courses</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+  								<tr>
+    								<td>Make-up Examination</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+  								<tr>
+    								<td>Last Date for Grades Submission of Make-up Exams, Conversion of I Grades, Graduate/Research/Progress Seminars</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+  								<tr>
+    								<td>Submission of grade moderation report from discipline</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+  								<tr>
+    								<td>Submission of moderation report by APCS</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+  								<tr>
+    								<td>Last date for Authentication of Grades</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+  								<tr>
+    								<td>Academic Pre-Registration for next semester</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  								<tr>
+    								<td>Vacation for UG students</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
+
+  									<tr>
+    								<td>Vacational leave applicable for faculty</td>
+    								<td>April 06-10, 2020 (Mon-Fri)</td>
+    								<td>M.tech + P.hD</td>
+    								<td>New UG</td>
+  								</tr>
 
 							</table>
+
+								<div class="calNotice">
+
+								<h4><sup>*</sup>It is mandatory to show answersheets and mark to students within 10 days of the evaluation</h4>
+
+
+								<table>
+
+									<tr>
+										<th colspan="3">Adjustment of Classes on Saturday</th>
+									</tr>
+
+									<tr>
+										<td>
+											Monday Time table
+										</td>
+										<td>
+											19/09/2020 & 24/10/2020
+										</td>
+										<td>
+											28/11/2020 & 16/01/2021
+										</td>
+									</tr>
+
+									<tr>
+										<td>
+											Tuesday Time table
+										</td>
+										<td>
+											26/09/2020 & 07/11/2020
+										</td>
+										<td>
+											05/12/2020 & 23/01/2021
+										</td>
+									</tr>
+
+									<tr>
+										<td>
+											Wednesday Time table
+										</td>
+										<td>
+											03/10/2020, 14/11/2020 & 05/12/2020
+										</td>
+										<td>
+											12/12/2020 & 30/01/2021
+										</td>
+									</tr>
+
+									<tr>
+										<td>
+											Thursday Time table
+										</td>
+										<td>
+											10/10/2020 & 21/11/2020
+										</td>
+										<td>
+											26/12/2020 & 06/02/2021
+										</td>
+									</tr>
+
+									<tr>
+										<td>
+											Friday Time table
+										</td>
+										<td>
+											17/10/2020 & 28/11/2020
+										</td>
+										<td>
+											02/01/2021 & 13/02/2021
+										</td>
+									</tr>
+
+								</table>
+
+							</div>
+							
 						</div>
 				</div>
 			</body>
