efa7f078f546428d9f30f8da973ee09687ad2e68
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-05-10 11:03:55 +0530

    fixed local Administration

--- en/lAdmins.php
+++ en/lAdmins.php
@@ -39,23 +39,22 @@
 				<button class="adminDetails">Training & Placement</button>
 					<div class="adminProfile">
 
+					  	<div class="profile">
 
-					  	<div class="profile"> 
 					  		<div class="localAdminProfile">
-    							<img src="./Images/ladmins/pka.png" alt="Vijay" >
-								<h5>Prof. Pritee Khanna</h5>
-								<h4>Chairperson</h4>
-							</div>
+    							<img src="./Images/ladmins/pka.png" alt="Preeti Khanna" >
+									<h5>Prof. Pritee Khanna</h5>
+									<h4>Chairperson</h4>
+								</div>
 					
-							<div class="localAdminProfile">
+								<div class="localAdminProfile">
 	    						<img src="./Images/ladmins/omvir.png" alt="Omvir Singh" >
-								<h5>Mr. Omvir Singh Bhadauria </h5>
-								<h4>Placement Officer</h4>
+									<h5>Mr. Omvir Singh Bhadauria </h5>
+									<h4>Placement Officer</h4>
+								</div>
+
 							</div>
 						</div>
-
-
-					</div>
 
 
 					<span class="br"></span>
@@ -77,65 +76,55 @@
 
 				<button class="adminDetails">Alumni Cell</button>
 					<div class="adminProfile">
-						<div class="rowDean">
-					  	<div class="columnDean">
+							<div class="localAdminProfile">
     						<img src="./Images/ladmins/puneetT.png" alt="Puneet T" >
 								<h5>Prof. Puneet Tandon</h5>
 								<h4>Professor In-charge</h4>
 							</div>
-						</div>
 					</div>
 
 					<span class="br"></span>
 
 				<button class="adminDetails">Innovation and Incubation Cell & Patent and Copyright Cell</button>
 					<div class="adminProfile">
-						<div class="rowDean">
-					  	<div class="columnDean">
+					  	<div class="localAdminProfile">
     						<img src="./Images/ladmins/rkjha.png" alt="Swapnali" >
 								<h5>Dr. Rakesh Kumar Jha </h5>
 								<h4>Professor In-charge</h4>
 							</div>
-						</div>
 					</div>
 
 					<span class="br"></span>
 
 				<button class="adminDetails">Communication Cell</button>
 					<div class="adminProfile">
-						<div class="rowDean">
-					  	<div class="columnDean">
+					  	<div class="localAdminProfile">
     						<img src="./Images/ladmins/varunB.png" alt="Varun B" >
 								<h5>Dr. Varun Bajaj</h5>
 								<h4>Professor In-charge</h4>
 							</div>
-						</div>
 					</div>
 
 					<span class="br"></span>
 
 				<button class="adminDetails">Science & Technology Counselor</button>
 					<div class="adminProfile">
-						<div class="rowDean">
-					  	<div class="columnDean">
+					  	<div class="localAdminProfile">
     						<img src="./Images/ladmins/akk.png" alt="Asish Kundu" >
 								<h5>Dr. Asish K. Kundu</h5>
 								<h4>Professor In-charge</h4>
 							</div>
-						</div>
 					</div>
 
 					<span class="br"></span>
 
 				<button class="adminDetails">Registrar (Acting)Finance & Accounts</button>
 					<div class="adminProfile">
-						<div class="rowDean">
-					  	<div class="columnDean">
+					  	<div class="localAdminProfile">
     						<img src="./Images/ladmins/swapnali.png" alt="Swapnali" >
 								<h5>Mrs. Swapnali Gadekar</h5>
 								<h4>Deputy Registrar</h4>
 							</div>
-						</div>
 					</div>
 
 
@@ -143,26 +132,22 @@
 
 				<button class="adminDetails">Establishment, General Administration and Internal Audit</button>
 					<div class="adminProfile">
-						<div class="rowDean">
-					  	<div class="columnDean">
+					  	<div class="localAdminProfile">
     						<img src="./Images/ladmins/rizwanA.png" alt="rizwan A" >
 								<h5>Mr. Rizwan Ahmed</h5>
 								<h4>Deputy Registrar</h4>
 							</div>
-						</div>
 					</div>
 
 					<span class="br"></span>
 
 				<button class="adminDetails">Academics, Student Affairs, Official Language Officer, CPIO</button>
 					<div class="adminProfile">
-						<div class="rowDean">
-					  	<div class="columnDean">
+					  	<div class="localAdminProfile">
     						<img src="./Images/ladmins/santosh.png" alt="santosh M" >
 								<h5>Mr. Santosh Mahobia</h5>
 								<h4>Assistant Registrar</h4>
 							</div>
-						</div>
 					</div>
 
 			</div>
