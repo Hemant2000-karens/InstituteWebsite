4cf12c5a8511ffa2d817536082120e92c8c33733
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-05-10 17:49:51 +0530

    c

--- en/hostel.php
+++ en/hostel.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<title>Academics | IIITDMJ</title>
+	<head>
+		<meta charset="utf-8">
+		<title>Academics | IIITDMJ</title>
 		<meta http-equiv="refresh" content="3000">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="./style.css">
-       <link rel="stylesheet" type="text/css" href="./mobile.css">
-</head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" type="text/css" href="./mobile.css">
+	</head>
 
-	    <?php include './topheader.php';?>
+	  <?php include './topheader.php';?>
 	   	<div class="container">
 	    	<body>
-	    		<?php include './Header.php';?>
+    		<?php include './Header.php';?>
 
 				<?php include "./navbar.php";?>
 
@@ -23,9 +23,9 @@
 				</div>
 
 				<div class="AcMain">
-
 						
-				</div>
+						
+			</div>
 
 
 			</body>
