ddf0195cf2c2380fa259aa07ec54dff2e01e4778
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-05-10 11:21:03 +0530

    c

--- en/admins.php
+++ en/admins.php
@@ -24,14 +24,11 @@
 				<br>
 				<button class="adminDetails">Director</button>
 					<div class="adminProfile">
-
-						<div class="rowDean">
-						  	<div class="columnDean">
-	    						<img src="./Images/DeanPnD/pnkon.png" alt="PnKondekar" >
-								<h5>Prof. P.N Kondekar</h5>
-								<h4>Director(Acting)/Dean(P&D)</h4>
-	  						</div>
-	  					</div>
+						<div class="localAdminProfile">
+	    				<img src="./Images/DeanPnD/pnkon.png" alt="PnKondekar" >
+							<h5>Prof. P.N Kondekar</h5>
+							<h4>Director(Acting)/Dean(P&D)</h4>
+	  				</div>
 					</div>
 
 
@@ -42,34 +39,32 @@
 					<div class="adminProfile">
 
 
-					  	<div class="rowDean"> 
-					  		<div class="columnDean">
+					  	<div class="gridprofile"> 
+					  		<div class="localAdminProfile">
     							<img src="./Images/DeanACD/vijayG.jpg" alt="Vijay" >
-								<h5>Prof. Vijay Kumar Gupta</h5>
-								<h4>Dean(Academics)</h4>
-							</div>
+									<h5>Prof. Vijay Kumar Gupta</h5>
+									<h4>Dean(Academics)</h4>
+								</div>
 					
-							<div class="columnDean">
+							<div class="localAdminProfile">
 	    						<img src="./Images/DeanStd/tanuja.png" alt="tanuj" >
 								<h5>Prof. Tanuja Sheorey</h5>
 								<h4>Dean(Students)</h4>
 							</div>
 
-							<div class="columnDean">
+							<div class="localAdminProfile">
 	    						<img src="./Images/DeanPnD/pnkon.png" alt="P.N Kondekar" >
 								<h5>Prof. P.N Kondekar</h5>
 								<h4>Dean(Planning & Development)</h4>
 							</div>
-					  	</div>
 
-						<div class="rowDean">
-							<div class="columnDean">
+							<div class="localAdminProfile">
     							<img src="./Images/DeanRSPC/pkpadhy.png" alt="Vijay" >
 								<h5>Prof. Prabin Kumar Padhhy</h5>
 								<h4>Dean(RSPC)</h4>
 							</div>
 
-							<div class="columnDean">
+							<div class="localAdminProfile">
 	    						<img src="./Images/DeanPnD/atul.png" alt="Vijay" >
 								<h5>Prof. Atul Gupta</h5>
 								<h4>Associate Dean(Planning & Development)</h4>
@@ -86,49 +81,44 @@
 				<button class="adminDetails">Head of Disciplines</button>
 					<div class="adminProfile">
 					  	
-						<div class="rowDean"> 
-					  		<div class="columnDean">
+						<div class="gridprofile"> 
+					  		<div class="localAdminProfile">
     							<img src="./Images/DeanPnD/atul.png" alt="Atul Gupta" >
 								<h5>Prof. Atul Gupta</h5>
 								<h4>CSE</h4>
 							</div>
 					
-							<div class="columnDean">
+							<div class="localAdminProfile">
 	    						<img src="./Images/Administrators/pkpadhy.png" alt="Dr.Prabir Mukhopadhyay" >
 								<h5>Dr. Prabir Mukhopadhyay</h5>
 								<h4>Design</h4>
 							</div>
 
-							<div class="columnDean">
+							<div class="localAdminProfile">
 	    						<img src="./Images/Administrators/anilkr.png" alt="Anil Kumar" >
 								<h5>Dr. Anil Kumar</h5>
 								<h4>ECE</h4>
 							</div>
-					  	</div>
 
-						<div class="rowDean">
-							<div class="columnDean">
+							<div class="localAdminProfile">
     							<img src="./Images/Administrators/pkjain.png" alt="Prashant" >
 								<h5>Prof. Prashant K. Jain</h5>
 								<h4>ME</h4>
 							</div>
 
-							<div class="columnDean">
+							<div class="localAdminProfile">
 	    						<img src="./Images/Administrators/mkroy.png" alt="Mukesh" >
 								<h5>Dr. Mukesh Kumar Roy</h5>
 								<h4>NS</h4>
 							</div>
 
-							<div class="columnDean">
+							<div class="localAdminProfile">
 	    						<img src="./Images/Administrators/puneetT.png" alt="Puneet" >
 								<h5>Prof. Puneet Tandon</h5>
 								<h4>Smart Manufacturing</h4>
 							</div>
 
-						</div>
-
-						<div class="rowDean">
-							<div class="columnDean">
+							<div class="localAdminProfile">
 								<img src="./Images/Administrators/sag.png" alt="Sunil" >
 								<h5>Dr. Sunil Agrawal</h5>
 								<h4>Liberal Arts</h4>
@@ -143,8 +133,8 @@
 
 				<button class="adminDetails">Registrar</button>
 					<div class="adminProfile">
-						<div class="rowDean">
-					  	<div class="columnDean">
+						<div class="gridprofile">
+					  	<div class="localAdminProfile">
     						<img src="./Images/Administrators/swapnali.png" alt="Swapnali" >
 								<h5>Mrs. Swapnali Gadekar</h5>
 								<h4>Deputy Registrar</h4>
@@ -169,19 +159,19 @@
 						<img src="./icons/locala.png"/>
 						<figcaption>Local Administration</figcaption>
 					</div>
-					</div>
-					<div class="otherPage">
-						<div class="card" onclick="linkOpen('ms')">
-							<img src="./icons/senate.png"/>
+
+					<div class="card" onclick="linkOpen('ms')">
+						<img src="./icons/senate.png"/>
 						<figcaption>Members of Senate</figcaption>
-						</div>
-						<div class="card" onclick="linkOpen('fc')">
-							<img src="./icons/finc.png"/>
+					</div>
+
+					<div class="card" onclick="linkOpen('fc')">
+						<img src="./icons/finc.png"/>
 						<figcaption>Finance Commitee</figcaption>
-						</div>
+					</div>
 						<div class="card" onclick="linkOpen('mv')">
 							<img src="./icons/mivi.png"/>
-						<figcaption>Mission & Vision</figcaption>
+							<figcaption>Mission & Vision</figcaption>
 						</div>
 				</div>
 			</section>
