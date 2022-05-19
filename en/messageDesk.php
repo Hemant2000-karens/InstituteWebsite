11cc9e961fe2e6fdcf40079723a57a0b830aed71
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-05-19 12:02:43 +0530

    fixed

--- en/messageDesk.php
+++ en/messageDesk.php
@@ -1,21 +1,23 @@
 <div class="messageDesk">
-	<div class="deskTitle">
-		<img src="./Images/Messages1.png">
-				Message from <strong>Director's Desk</strong>
-	</div>
+	<div class="messageDesk_main">
+		<div class="deskTitle">
+			<img src="./Images/Messages1.png">
+					Message from <strong>Director's Desk</strong>
+		</div>
 
-	<div class="messageDeskCombined">
-			<img class="imagedirector" src="./Images/director_msg.jpeg" alt="Director Image" width="600" height="400">
+		<div class="messageDeskCombined">
+				<img class="imagedirector" src="./Images/director_msg.jpeg" alt="Director Image" width="600" height="400">
 
-		<div class="deskSpeech">
+			<div class="deskSpeech">
 
 
-			<img src="./icons/opquote.png" style = "height:20px; width: 20px">
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				<img src="./icons/opquote.png" style = "height:20px; width: 20px">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 
-		<img src="./icons/clquote.png" style ="height:20px; width:20px;">
-		 ~ (Acting)
-		
+			<img src="./icons/clquote.png" style ="height:20px; width:20px;">
+			 ~ (Acting)
+			
+			</div>
 		</div>
 	</div>
 </div>
