1b7ffbb412ca0850da88875e19c2e4ba89778670
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-05-26 13:03:11 +0530

    update

--- en/faculty_profilePage.php
+++ en/faculty_profilePage.php
@@ -3,9 +3,16 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
+	<title><?php  ;?></title>
+	<link rel="stylesheet" type="text/css" href="facultyPage.css">
 </head>
 <body>
-
+	<div class="faculty_Nav">
+        <img src="./logo_college.png" alt="header"/>
+        <h3>IIITDM JABALPUR</h3>
+	</div>
+	<div class="container">
+		
+	</div>
 </body>
-</html>
+</html> 
