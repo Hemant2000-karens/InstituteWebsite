558c8216061317f59c592a298b597b8581ab949d
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-06-03 20:30:51 +0530

    completed odd sem

---
+++ admission/curSem7.php
@@ -0,0 +1,41 @@
+<?php include '../en/connectionDB.php'; ?>
+
+<table class="table">
+	<caption>Semester 1</caption>
+
+	<thead>
+		<tr>
+	    	<th scope="col">Code</th>
+	    	<th scope="col">Course Name</th>
+	    	<th scope="col">Offered By</th>
+	    	<th scope="col">Discipline</th>
+	  	</tr>
+	</thead>
+	<tbody>
+
+		<?php 
+		$query = "Select * from curriculum where semester = 7";
+
+		$result = $link->query($query);
+		if($result -> num_rows > 0)
+			{
+				while($rows = $result -> fetch_assoc())
+				{
+					echo ("<tr>
+					<td style='color:".$rows['color']."'>".$rows['code']."</td>
+					<td style='color:".$rows['color']."'>".$rows['CourseName']."</td>
+					<td style='color:".$rows['color']."'>".
+					$rows['offerredBy']."</td>
+					<td style='color:".$rows['color']."'>".$rows['Discipline']."</td>
+					</tr>");
+				}
+			}
+			?>
+            
+        </tbody>
+  	<!-- Heading of the tables-->
+
+</table>
+
+
+<?php $link -> close();?>
