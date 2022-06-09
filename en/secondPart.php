8f4c38426ed8771cfaae6602c8bbe4ee7cffd9e2
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-06-09 22:19:57 +0530

    gith

--- en/secondPart.php
+++ en/secondPart.php
@@ -6,16 +6,24 @@
 
 
 		<div class="aboutUsMain">
-			<div class="aboutSub">
-				<h2>About IIITDMJ</h2>
+			<div class="aboutBackground">
+				<div class="aboutSub">
+					<h2>About IIITDMJ</h2>
 
-				<div class="aboutContent">
-					<p>
-						
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					<div class="aboutContent">
+				<p>
+				Founded in 2005 as the Jabalpur Engineering College, Indian Institute of Information Technology, Jabalpur focuses on Information Technology enabled Design and Manufacturing. It is a Institute of National Importance under the IIIT Act of the Government of India. The institute appears in the list of top 100 engineering colleges in India.
 
 				</p>
+				<p>
+				The institute offers BTech program for computer science engineering, mechanical engineering and electronics and communication engineering and Bachelor course in Design.
+				</p>
+				<p>
+				Undergraduate students are admitted through Joint Entrance Examination(Main) (JEE MAIN) and the UCEED, while post graduate students are admitted through GATE( for MTech Courses) and CEED( for Design course).
+						</p>
+					</div>
 				</div>
+
 			</div>
 		</div>
 
