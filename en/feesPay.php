e592960a05b89ca65b6cb4aeba30e4714eecace6
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-06-14 01:03:12 +0530

    update
    Added Anti Ragging, and fees Pay

---
+++ en/feesPay.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="icon" type="jpg/png" href="./logo_college.png" sizes="16x16">
+	<title>Academics | IIITDMJ</title>
+		<meta http-equiv="refresh" content="3000">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./style.css">
+       <link rel="stylesheet" type="text/css" href="./mobile.css">
+</head>
+
+	    <?php include './topheader.php';?>
+	   	<div class="container">
+	    	<body>
+	    		<?php include './Header.php';?>
+
+				<?php include "./navbar.php";?>
+
+				<span class="br"></span>
+	
+				<div class="aboutHeader" id="adminHeader">
+					<h2>Fees Payment</h2>
+				</div>
+				
+				<div class="AcMain">
+
+
+
+
+
+				</div>
+			</body>
+		</div>
+
+<?php include './footer.php'?>
+<script>
+
+	window.onscroll = function()
+{
+  stickyFunction()
+};
+
+
+var nbar = document.getElementById("adminHeader");
+var sticky = nbar.offsetTop;
+function stickyFunction() {
+  if (window.pageYOffset >= sticky) {
+    nbar.classList.add("sticky");
+  } else {
+    nbar.classList.remove("sticky");
+ 		}
+	}
+
+var coll = document.getElementsByClassName("acdpB");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("show");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+</script>
+</html>
