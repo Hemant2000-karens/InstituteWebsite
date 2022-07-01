0c63186ddeaa7b431c5b4ecd92489a5eb385ee6b
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-07-01 13:01:10 +0530

    update

--- en/Header.php
+++ en/Header.php
@@ -1,8 +1,10 @@
 <div class="container">
     <div class="header">
-        <img src="logo_college.png" alt="header"/>
-        <h1>
-        PDPM<br>Indian Institute of Information Technology,<br>Design and Manufacturing, Jabalpur</h1>
+        <a href="./en">
+            <img src="logo_college.png" alt="header"/>
+            <h1>
+            PDPM<br>Indian Institute of Information Technology,<br>Design and Manufacturing, Jabalpur</h1>
+        </a>
     </div>
 </div>
 <hr>
