cf6c307c8d546cdc647aa1980e64d3f4a4f4be0f
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-07-04 10:38:46 +0530

    bugs fix

--- en/Header.php
+++ en/Header.php
@@ -2,9 +2,9 @@
     <div class="header">
         <a href="./index.php">
             <img src="logo_college.png" alt="header"/>
+        </a>
             <h1>
             PDPM<br>Indian Institute of Information Technology,<br>Design and Manufacturing, Jabalpur</h1>
-        </a>
     </div>
 </div>
 <hr>
