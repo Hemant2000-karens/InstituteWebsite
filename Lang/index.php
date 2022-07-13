af72d0d70a7554ca45c45f329aa027b536c4da46
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-07-13 16:41:31 +0530

    update

--- Lang/index.php
+++ Lang/index.php
@@ -6,21 +6,41 @@
     </head>
 
     <body>
-      <!-- <div id="overlay" onclick="off()">
-        <img  id="welcomeNotice" src="../Images/csir.jpg">
-    </div> -->
     <?php include 'topheader.php';?>
     <?php include 'Header.php'?>
   <div class="container">
       
-      
+      <div class="aboutHeader" id="adminHeader">
+          <h2>Choose Your Language</h2>
+        </div>
+
+        <span class="br"></span>
+
+        <div class="AcMain">
+
+          <div class="Language">
+
+            <a href="../en-in/">English</a>
+            <a href="../en-hi/">हिन्दी</a>
+            <a href="../en-bn/">বাংলা</a>
+            <a href="../en-in/">ଓଡିଆ</a>
+            <a href="../en-in/">ਪੰਜਾਬੀ</a>
+            <a href="../en-in/">ગુજરાતી</a>
+            <a href="../en-mh/">मराठी</a>
+            <a href="../en-in/">ಕನ್ನಡ</a>
+            <a href="../en-in/">తెలుగు</a>
+            <a href="../en-in/">தமிழ்</a>
+            <a href="../en-in/">മലയാളം</a>
+            <a href="../en-in/">اردو(Urdu)</a>
+            <a href="../en-in/">অসমীয়া</a>
+            <a href="../en-in/">اردو(Urdu)</a>
+
+          </div>
 
 
+        </div>
   </div>
 
 </body>
 <?php include './footer.php';?>
-<script type="text/javascript" src="./script.js"></script>
-
-<script type="text/javascript" src="./script1.js"></script>
 </html>
