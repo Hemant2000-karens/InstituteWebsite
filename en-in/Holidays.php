ee4508b04957d5427df2d1c3d3b5af79af6c8214
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-07-18 23:46:49 +0530

    bug fix
    Completed seonday nav campus tour,
    and Added Holidays list,
    and fixed table td width

--- en-in/Holidays.php
+++ en-in/Holidays.php
@@ -9,7 +9,7 @@
       <div class="container">
         <body>
         <?php include './Header.php';?>
-
+        <?php include './connectionDB.php';?>
         <?php include "./navbar.php";?>
 
         <span class="br"></span>
@@ -19,12 +19,17 @@
         </div>
 
 
+        <?php 
+          $sqlQuery = "SELECT year,filename from holiday where type = 'closed';";
+          $result = $link->query($sqlQuery);
+        ?>
+
         <br/>
 
         <div class="cal_Nav">
-            <button class="calTab" onclick="changeCal(event, 'agenda')" id="defaultOpen">Closed Holidays
+            <button class="calTab" onclick="changeCal(event, 'closedHoliday')" id="defaultOpen">Closed Holidays
             </button>
-            <button class="calTab" onclick="changeCal(event, 'minute')">Restricted Holidays
+            <button class="calTab" onclick="changeCal(event, 'restrictedHoliday')">Restricted Holidays
             </button>
           </div>
 
@@ -32,17 +37,63 @@
         <div class="AcMain">
           
 
-          
+          <div class="calTable" id="closedHoliday">
+
+            <table>
+              <tr>
+                <th>Year</th>
+              </tr>
+
+                    <?php 
+
+                      if($result -> num_rows > 0)
+                      {
+                        while($rows = $result -> fetch_assoc())
+                        {
+                          echo ("<tr>
+                                <td><a href = './Documents/Holidays/closed".$rows['filename']."'>".$rows['year']."</a></td>
+                                </tr>");
+                        }
+                      }
+                    ?>
+
+            </table>
+
+          </div>
 
 
+          <div class="calTable" id="restrictedHoliday">
 
+              <table>
+                <tr>
+                  <th>Year</th>
+                </tr>
+
+                 <?php 
+                        $sqlQuery = "SELECT year,filename from holiday where type = 'restricted';";
+                        $result = $link->query($sqlQuery);
+                      if($result -> num_rows > 0)
+                      {
+                        while($rows = $result -> fetch_assoc())
+                        {
+                          echo ("<tr>
+                                <td><a href = './Documents/Holidays/restricted".$rows['filename']."'>".$rows['year']."</a></td>
+                                </tr>");
+                        }
+                      }
+                    ?>
+
+              </table>
+
+          </div>
 
         </div>
             
       </body>
     </div>
-
       <?php include './footer.php'?>
+
+      <?php mysqli_close($link);?>
   <script type="text/javascript" src="./script.js"></script>
-  <script type="text/javascript" src="./script.js"></script>
+  <script type="text/javascript" src="./script1.js"></script>
 </html>
--- en-in/convocation.php
+++ en-in/convocation.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 	<html>
 		<head>
-			<?php include_once './headTag.php';?>
+			<?php include_once './headTag.php'; ?>
 			<title>Convocation | IIITDMJ</title>
 		</head>
 
-	    <?php include './topheader.php';?>
+	    <?php include './topheader.php'; ?>
 	   	<div class="container">
 	    	<body>
-	    	<?php include './Header.php';?>
+	    	<?php include './Header.php'; ?>
 
 				<?php include "./navbar.php";
-				include './connectionDB.php';?>
+				include './connectionDB.php'; ?>
 
 				<span class="br"></span>
 	
@@ -23,48 +23,22 @@
 
 				<div class="AcMain">
 					
+					
 
-						<div class="calTable" id="odd">
-							
 
-							<?php 
-							$sql = "SELECT * from academicCalendar where semester = 1"; 
-							$result = $link->query($sql);
-							?>
+					
 
 
 
 
-							<table id="tb1">
-								<tr>
-								  	<th>Circulars</th>
-								  	<th>Date</th>
-  								</tr>
-  								<!-- Heading of the tables-->
 
 
-										<?php
 
-  											if($result -> num_rows > 0)
-											{
-												while($rows = $result -> fetch_assoc())
-												{
-													echo ("<tr>
-														<td>".$rows['event']."</td>
-														<td>".$rows['dateFromNewUG']." to ".$rows['dateToNewPG']."</td>
-														</tr>");
-												}
-											}
 
-										?>
-
-							</table>
-						</div>
 				</div>
 			</body>
 		</div>
-					<? mysqli_close($link); ?>
-			<?php include './footer.php'?>
+<?php include './footer.php' ?>
 <script type="text/javascript" src="./script.js"></script>
 <script type="text/javascript" src="./script1.js"></script>
 </html>
