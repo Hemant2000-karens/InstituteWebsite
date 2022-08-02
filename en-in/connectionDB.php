4a9fd8619cb58e494306075d2cd891b97435d050
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-08-02 15:46:21 +0530

    Update connectionDB.php

--- en-in/connectionDB.php
+++ en-in/connectionDB.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect("localhost:3306", "website", "password", "Institute");
+$link = mysqli_connect("localhost:3306", "website", "passWord@123", "Institute");
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
