3e412561b27dc61f413c462c2d4d7d9921af9d2e
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-09-01 15:21:39 +0530

    submit

--- en-in/ofcstff.php
+++ en-in/ofcstff.php
@@ -8,9 +8,7 @@
 <div class = "container">
 <body>
 
-	<?php include "./Header.php"; ?>
-
-	<?php include "./navbar.php"; ?>
+	<?php include './header2.php';?>
 	<span class="br"></span>
 	
 				<div class="aboutHeader" id="adminHeader">
@@ -53,6 +51,23 @@
 
 <?php include './footer.php' ?>
 
-
 <script type="text/javascript" src="./script.js"></script>
+
+
+<script type="text/javascript">
+  var indexNavBar = document.getElementById("mainNav");
+  var stickynav = indexNavBar.offsetTop;
+    window.onscroll = function()
+    {
+      stickyNavbar()
+    };
+
+    function stickyNavbar() {
+    if (window.pageYOffset >= stickynav) {
+      indexNavBar.classList.add("indexNav2");
+    } else {
+      indexNavBar.classList.remove("indexNav2");
+    }
+  }
+</script>
 </html>
