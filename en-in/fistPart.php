388422b75e5c682a36a5c544e5ea8b5a9061ca9a
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2022-09-27 16:46:32 +0530

    Update fistPart.php

--- en-in/fistPart.php
+++ en-in/fistPart.php
@@ -5,109 +5,79 @@
 
 
 
-		<div class="aboutUsMain">
-			<div class="aboutBackground">
-				<div class="aboutSub">
-					<h2>New/Events</h2>
+		<div class="campusTour">
+				<div class="campusTour_sub">
+					<h2>News/Events</h2>
+				</div>
+				
+					<div class="scrollbar_main Page">
 
-					<div class="aboutContent">
-				<p>
-				Founded in 2005 as the Jabalpur Engineering College, Indian Institute of Information Technology, Jabalpur focuses on Information Technology enabled Design and Manufacturing.
 
-					It is a Institute of National Importance under the IIIT Act of the Government of India. The institute appears in the list of top 100 engineering colleges in India.
-				</p>
+
 					</div>
+
 					<center>
 						<button onclick="viewMore()" id="viewAllButton">Read More</button>
 					</center>
+
+			</div>	
+
+
+
+		<div class="campusTour">
+				<div class="campusTour_sub">
+					<h2>Placement</h2>
 				</div>
-
-			</div>
-		</div>
-
+				
+					<div class="scrollbar_main Page">
 
 
-		<div class="messageDesk">
-			<div class="messageDesk_main">
-			<div class="deskTitle">
-				<strong>Placement</strong>
-			</div>
 
-			<div class="messageDeskCombined">
-				<img class="imagedirector" src="../Images/director_msg.jpeg" alt="Director Image" width="600" height="400">
-
-			<div class="deskSpeech">
-
-
-				<img src="../icons/opquote.png" style = "height:20px; width: 20px">
-			I hope that our students and faculty will carry the flag of IIITDM Jabalpur to greater heights by applying their knowledge in an inter-disciplinary manner to provide solutions for various industrial, research and development projects. I hope they will stand as responsible and dedicated technocrats in the nation building.
-			<img src="../icons/clquote.png" style ="height:20px; width:20px;">
-			 ~ (Acting)
-			
 					</div>
-				</div>
-			</div>
-		</div>
 
+					<center>
+						<button onclick="viewMore()" id="viewAllButton">Read More</button>
+					</center>
+
+			</div>	
 
 
 
 
 			<div class="campusTour">
-			<div class="campusTour_sub">
-				<h2>Academics</h2>
-			</div>
-			<div class="campusDetail">
-
-				<!-- <div class="campus-pill hostels" onclick="openCampusTour('hostel')">
-
-					<div class="index-title">
-						<h4>Hostels</h4> <i class="bi bi-arrow-up-right-circle"></i>
-					</div>
-
+				<div class="campusTour_sub">
+					<h2>Academics</h2>
 				</div>
-
-				<div class="campus-pill health" onclick="openCampusTour('phc')">
-
-					<div class="index-title">
-						<h4>Hospital</h4> <i class="bi bi-arrow-up-right-circle"></i>
-					</div>
-
-				</div>
-
-				<div class="campus-pill mess"
-				onclick="openCampusTour('mess')">
-					<div class="index-title">
-						<h4>Central Mess</h4> <i class="bi bi-arrow-up-right-circle"></i>
-					</div>
-				</div>
-
-				<div class="campus-pill visitors"
-				onclick="openCampusTour('visitorCenter');">
-					<div class="index-title">
-						<h4>Visitors Center</h4> <i class="bi bi-arrow-up-right-circle"></i>
-					</div>
-				</div> -->
-			</div>
-		</div>	
-
-
-		<div class="reseachHighLights">
-			<div class="reseachHighLights_sub">
-				<h2>Tender/Notices</h2>
-				<p><a href="./viewEvents.php"/>View All Research Highlights</a></p>
-			</div>
-
-			<div class="rContentMain">
 				
+					<div class="scrollbar_main Page">
+
+
+
+					</div>
+
+					<center>
+						<button onclick="viewMore()" id="viewAllButton">Read More</button>
+					</center>
+
+			</div>	
+
+
+		<div class="campusTour">
+				<div class="campusTour_sub">
+					<h2>Tender/Notices</h2>
+				</div>
 				
+					<div class="scrollbar_main Page">
 
-			</div>
 
 
+					</div>
+
+					<center>
+						<button onclick="viewMore()" id="viewAllButton">Read More</button>
+					</center>
+
+			</div>	
 
 		</div>
-
-
 	</div>
-</div>
